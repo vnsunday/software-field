8b57bfb2c9386843b32c592f6588e4f1d82a1ac8
vnsunday <truyenspidermanvn@gmail.com>
2026-08-23 14:18:26 +0700

    periodic string

--- applications/strings/strings.cpp
+++ applications/strings/strings.cpp
@@ -1,7 +1,59 @@
+#include <cstdlib>
+#include <ctime>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <string>
+
+using namespace std;
+
+/*============================================================
+    bEntire = true: accept a suffix equals the Entire string 
+ *============================================================*/
+std::string a_suffix(std::string str, bool bEntire = false) {
+    int nL = str.length();
+
+    if (nL > 0) {
+        int nS = std::rand() % (nL);
+        return str.substr(nL - nS - 1);
+    }
+    else {
+        return string("");
+    }
+}
+
+/*============================================================
+    Generating a semi-periodic string from a period
+ *============================================================*/
+std::string generate_semiperiodic(const char* period) {
+    std::string ssf = a_suffix(period, true); // Suffix or Entire string
+    int ncopy = 1 + std::rand() % 5;
+
+    std::string str = ssf;
+    for (int i=0; i<ncopy;i++) {
+        str += period;
+    }
+
+    return str;
+}
+
+/*============================================================
+    Generating a periodic string from a period
+ *============================================================*/
+ std::string generate_periodic(const char* period) {
+int ncopy = 2 + std::rand() % 5;
+
+    std::string str = "";
+    for (int i=0; i<ncopy;i++) {
+        str += period;
+    }
+
+    return str;
+ }
 
 int main() {
+    std::srand(std::time(NULL)); // Random seed
+
     const char* szAuthor[] = {
         "A", 
         "M"
@@ -48,5 +100,15 @@ int main() {
     int n1 = sizeof(azBioMR) / sizeof(azBioMR[0]);
     int n2 = sizeof(azBioPR) / sizeof(azBioPR[0]);
     printf("Array Length=%d; %d; %d\r\n", n, n1, n2);
+
+
+    // Periodic testing 
+    std::string period = "abc";
+    std::string strprd = generate_periodic("abc");
+    std::string strsmprd = generate_semiperiodic("abc");
+
+    printf("Semi periodic of %s: %s\r\n", period.c_str(), strsmprd.c_str());
+    printf("Periodic of %s: %s\r\n", period.c_str(), strprd.c_str());
+
     return 0;
 }
